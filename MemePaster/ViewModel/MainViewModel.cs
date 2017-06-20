78fa173fddca3e0afa4016e5d9f66f6ae2afd24f
Alexey K. <lemontloco@gmail.com>
2017-06-21 00:11:48 +0300

    Added about and load windows and some viewmodels

--- MemePaster/ViewModel/MainViewModel.cs
+++ MemePaster/ViewModel/MainViewModel.cs
@@ -1,5 +1,12 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using MemePaster.Model;
+using System.Windows;
+using System;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+using MemePaster.Util;
+using System.Windows.Input;
 
 namespace MemePaster.ViewModel
 {
@@ -8,8 +15,53 @@ namespace MemePaster.ViewModel
         public MainViewModel()
         {
             WindowTitle = "Meme Paster";
+            AddMemeCommand = new RelayCommand(AddMemeAction);
+            AboutCommand = new RelayCommand(ShowAboutWindowAction);
         }
+
+        public RelayCommand AddMemeCommand { get; private set; }
+        public RelayCommand AboutCommand { get; private set; }
+        public RelayCommand CopyMemeCommand { get; private set; }
         public string WindowTitle { get; set; }
         public Settings Settings { get; set; }
+
+        private void AddMemeAction()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Open Meme";
+            dialog.Filter = "jpg images (*.jpg)|*.jpg|png images (*.png)|*.png|bmp images (*.bmp)|*.bmp";
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog() == true)
+            {
+                foreach (var memeImage in dialog.FileNames)
+                {
+                    // get meme image
+                    var bmImage = new BitmapImage(new Uri(memeImage));
+                    // change it`s size
+                    bmImage = ImageManager.ChangeImageSize(Settings.MemeImageWidth, Settings.MemeImageHeight, bmImage);
+                    // save as MemeCell
+                    Settings.MemeCells.Add(new MemeCell(Converter.ImageToBytes(bmImage)));
+                }
+            }
+        }
+        private void ShowAboutWindowAction()
+        {
+            
+        }
+        private void CopyMemeAction()
+        {
+            //if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            //{
+            //    foreach (var item in Settings.MemeCells)
+            //    {
+            //        if (item.Image == (sender as MemeButton).MemeCell.Image)
+            //        {
+            //            Settings.MemeCells.Remove(item);
+            //            break;
+            //        }
+            //    }
+            //}
+            //Clipboard.SetImage(Converter.BytesToImage(((sender as MemeButton).MemeCell.Image)));
+        }
     }
 }
--- MemePaster/ViewModel/ViewModelLocator.cs
+++ MemePaster/ViewModel/ViewModelLocator.cs
@@ -18,29 +18,19 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace MemePaster.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<AboutViewModel>();
+            SimpleIoc.Default.Register<LoadViewModel>();
         }
-
-        public MainViewModel Main
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
-        
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+        public AboutViewModel About => ServiceLocator.Current.GetInstance<AboutViewModel>();
+        public LoadViewModel Load => ServiceLocator.Current.GetInstance<LoadViewModel>();
+  
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
